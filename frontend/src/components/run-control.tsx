6954e8f86ee77da32a0accb8732cad0fb80673b8
skowrons <jakub.skowronskI@pd.infn.it>
2024-11-02 02:10:33 +0100

    Better start current after the run started.

--- frontend/src/components/run-control.tsx
+++ frontend/src/components/run-control.tsx
@@ -421,12 +421,10 @@ export function RunControl() {
         return
       }
 
-      // If save data is on, startAcqusiitonCurrent
+      await startRunProcess()
       if (saveData) {
         await startAcquisitionCurrent(runNumber.toString())
       }
-
-      await startRunProcess()
       const initialCurrent = await getDataCurrent()
       localStorage.setItem('initialBeamCurrent', initialCurrent.toString())
     } catch (error) {
